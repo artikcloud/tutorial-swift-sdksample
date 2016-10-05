0a8d9a3eb7aba4554ff52dc4e96a08f3fc9753f6
jonathanpaek <paekjt+dev@gmail.com>
2016-10-04 20:02:01 -0700

    adds initial sample code and project files
    swift ignores files

---
+++ tutorial-swift-artikcloud-sdksample/Template_Config.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  tutorial-swift-artikcloud-sdksample
+//
+//  Created by Jonathan Paek on 9/20/16.
+//  Copyright © 2016 ARTIK Cloud. All rights reserved.
+//
+
+
+// file should be renamed from TemplateConfig.swift to Config.swift
+// class name should be renamed from TemplateConfig to Config
+public class TemplateConfig {
+    
+    static let DEVICEID = "Your Device Id"
+    static let DEVICETOKEN = "Your Device Token"
+    
+    init(){ }
+    
+}
